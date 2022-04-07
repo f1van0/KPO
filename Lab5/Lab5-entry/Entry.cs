4f74cb2adbeb816634a34e4a466192359585e97e
fivan <46835435+f1van0@users.noreply.github.com>
2022-04-07 21:47:30 +0700

    save some changes

--- Lab5/Lab5-entry/Entry.cs
+++ Lab5/Lab5-entry/Entry.cs
@@ -13,37 +13,37 @@ using Lab5_entry.IPC;
 
 
 namespace Lab5_entry
-	{
+{
 	public partial class Entry : Form
-		{
+	{
 		Socket socket;
 		string currentImageName;
 
-		public Entry ()
-			{
+		public Entry()
+		{
 			InitializeComponent();
-			}
+		}
 
-		private void Entry_Load (object sender, EventArgs e)
-			{
+		private void Entry_Load(object sender, EventArgs e)
+		{
 
-			}
+		}
 
-		private void postTextButton_Click (object sender, EventArgs e)
-			{
+		private void postTextButton_Click(object sender, EventArgs e)
+		{
 			if (textBox1.Text != "")
-				{
+			{
 				//sendColor(Selected, "Storage");
 				sendText(textBox1.Text, "Preview");
-				}
 			}
+		}
 
-		private void sendText (string str, string windowTitle)
-			{
+		private void sendText(string str, string windowTitle)
+		{
 			IntPtr ptrWnd = NativeMethods.FindWindow(null, windowTitle);
 			IntPtr ptrCopyData = IntPtr.Zero;
 			try
-				{
+			{
 				// Create the data structure and fill with data
 				NativeMethods.COPYDATASTRUCT copyData = new NativeMethods.COPYDATASTRUCT();
 				copyData.dwData = new IntPtr(2);        //type of data
@@ -53,33 +53,33 @@ namespace Lab5_entry
 				// Allocate memory for the data and copy
 				ptrCopyData = Marshal.AllocCoTaskMem(Marshal.SizeOf(copyData));
 				Marshal.StructureToPtr(copyData, ptrCopyData, false);
-
+				
 				// Send the message
 				NativeMethods.SendMessage(ptrWnd, NativeMethods.WM_COPYDATA, IntPtr.Zero, ptrCopyData);
-				}
-			catch ( Exception ex )
-				{
-				MessageBox.Show(ex.ToString(), "Entry WM_COPYDATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-			finally
-				{
-				// Free the allocated memory after the contol has been returned
-				if ( ptrCopyData != IntPtr.Zero )
-					Marshal.FreeCoTaskMem(ptrCopyData);
-				}
 			}
-
-		private void SetImageButton_Click (object sender, EventArgs e)
-		{
-			if ( openFileDialog1.ShowDialog() == DialogResult.OK )
-            {
-				currentImageName = openFileDialog1.FileName;
-				pictureBox1.Image = new Bitmap(currentImageName);
-			}	
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), "Entry WM_COPYDATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			finally
+			{
+				// Free the allocated memory after the contol has been returned
+				if (ptrCopyData != IntPtr.Zero)
+					Marshal.FreeCoTaskMem(ptrCopyData);
+			}
 		}
 
-        private void SendImageButton_Click(object sender, EventArgs e)
-        {
+		private void SetImageButton_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				currentImageName = openFileDialog1.FileName;
+				pictureBox1.Image = new Bitmap(currentImageName);
+			}
+		}
+
+		private void SendImageButton_Click(object sender, EventArgs e)
+		{
 			new Task(() =>
 			{
 				try
@@ -95,16 +95,16 @@ namespace Lab5_entry
 			}).Start();
 		}
 
-        private void CopyStructureButton_Click(object sender, EventArgs e)
-        {
+		private void CopyStructureButton_Click(object sender, EventArgs e)
+		{
 			DataFormats.Format fullnameFormat = DataFormats.GetFormat("FullName");
 			FullName fullName = new FullName(FirstNameTextBox.Text, SecondNameTextBox.Text, ThirdNameTextBox.Text);
 			DataObject fullNameDataObject = new DataObject(fullnameFormat.Name, fullName);
 			Clipboard.SetDataObject(fullNameDataObject);
 		}
 
-        private void PasteStructureButton_Click(object sender, EventArgs e)
-        {
+		private void PasteStructureButton_Click(object sender, EventArgs e)
+		{
 			DataFormats.Format fullnameFormat = DataFormats.GetFormat("FullName");
 			IDataObject retrievedObject = Clipboard.GetDataObject();
 			FullName receivedFullName = (FullName)retrievedObject.GetData(fullnameFormat.Name);
@@ -117,21 +117,21 @@ namespace Lab5_entry
 			}
 		}
 
-        private void CopyImageButton_Click(object sender, EventArgs e)
-        {
+		private void CopyImageButton_Click(object sender, EventArgs e)
+		{
 			if (pictureBox1.Image != null)
-            {
+			{
 				Clipboard.SetImage(pictureBox1.Image);
 			}
 		}
 
-        private void CopyText_Click(object sender, EventArgs e)
-        {
+		private void CopyText_Click(object sender, EventArgs e)
+		{
 			if (textBox1.Text != "")
 			{
 				Clipboard.SetText(textBox1.Text);
 			}
 		}
-    }
-
 	}
+
+}
